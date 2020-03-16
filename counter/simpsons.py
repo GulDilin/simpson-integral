5a6dadd0ac37ddf26e0e28c0fb162f7bba93491d
GulDilin <zhenyagurin@gmail.com>
2020-03-17 00:53:32 +0300

    Initial commit

---
+++ counter/simpsons.py
@@ -0,0 +1,32 @@
+from math import sin, log
+
+
+class Integral:
+    def __init__(self, func, e, left, right):
+        self.func = func
+        self.e = e
+        self.left = left
+        self.right = right
+
+    def calculate(self):
+        n = 2
+        e = None
+        old_res = None
+        res = None
+
+        while e is None or e > self.e:
+            h = abs(self.right - self.left) / n / 2
+            res = self.func(self.left) + 4 * sum([self.func(self.left + h * (2 * i - 1)) for i in range(1, n + 1)])
+            res += 2 * sum([self.func(self.left + h * (2 * i)) for i in range(1, n)]) + self.func(self.right)
+            res *= h / 3
+            e = abs(res - old_res) / 15 if old_res else None
+            old_res = res if not old_res else old_res
+            n *= 2
+        return res
+
+
+if __name__ == '__main__':
+    funcs = [[lambda x: 5 / x, "5/x"], [lambda x: sin(x), "sin(x)"], [lambda x: log(x), "ln(x)"]]
+    for [func, name] in funcs:
+        integral = Integral(func, 0.001, 0, 1)
+        print(f'{name} from {integral.left} to {integral.right} = {integral.calculate()}')
